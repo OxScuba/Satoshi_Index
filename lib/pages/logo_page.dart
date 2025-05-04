fc423ef5a3b85043ff9756d9fb871a9b79d0a632
OxScuba <fernandez.marco@outlook.fr>
2025-05-04 20:19:08 +0200

    update readme

--- lib/pages/logo_page.dart
+++ lib/pages/logo_page.dart
@@ -10,11 +10,20 @@ class LogoPage extends StatelessWidget {
         title: const Text('Logo Satoshi Index'),
         backgroundColor: Colors.orange,
       ),
-      body: Center(
-        child: Image.asset(
-          'lib/assets/images/logo.png',
-          width: 200,
-          height: 200,
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Image.asset('lib/assets/images/logo.png', width: 200, height: 200),
+            const SizedBox(height: 24),
+            const Text(
+              "Satoshi Index est une application pédagogique qui permet de visualiser l'évolution des prix des produits du quotidien en euros (€) et en bitcoins (₿), exprimés en satoshis. "
+              "Elle propose des graphiques interactifs, un tableau de données trimestrielles, et permet de mieux comprendre l’impact de l’inflation ainsi que le pouvoir d’achat à travers le prisme du Bitcoin.",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 16),
+            ),
+          ],
         ),
       ),
     );
