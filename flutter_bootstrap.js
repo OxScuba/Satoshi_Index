48085e0dd1b337d8ffee88f002eeee1468b2f5d9
OxScuba <fernandez.marco@outlook.fr>
2025-07-12 23:37:07 +0200

    Déploiement web sur gh-pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "802629234"
+    serviceWorkerVersion: "480914682"
   }
 });
