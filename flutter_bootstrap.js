e13b6533281fb15b23510628d20f2ba543738b6d
OxScuba <fernandez.marco@outlook.fr>
2026-07-17 21:33:03 +0200

    add English and Spanish Localization

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3397971115"
+    serviceWorkerVersion: "162005391"
   }
 });
